4bf22d1b1c33b106d428fcff3d57c81c0419375f
Te Wei <tw2159@nyu.edu>
2021-10-17 17:08:05 -0700

    update

--- server.js
+++ server.js
@@ -81,3 +81,20 @@ app.get('/api/animals/:id', (req, res) => {
     res.json(result);
   } else {
     res.send(404);
+  }
+});
+
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
